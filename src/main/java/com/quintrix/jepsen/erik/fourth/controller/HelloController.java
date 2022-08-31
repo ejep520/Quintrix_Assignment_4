79a28ff26696d76c1cd47eb046a740134c791ade
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-08-31 10:59:15 -0700

    added a post mapping

--- src/main/java/com/quintrix/jepsen/erik/fourth/controller/HelloController.java
+++ src/main/java/com/quintrix/jepsen/erik/fourth/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.quintrix.jepsen.erik.fourth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,12 @@ public class HelloController {
   public Person helloWorld(@RequestParam String name, @RequestParam Person personName) {
     personName.setFirstName(name);
     return personName;
+  }
+
+  @PostMapping("/post-example")
+  @ResponseBody
+  public Person postHello(@RequestParam String fName, @RequestParam Person lName) {
+    lName.setFirstName(fName);
+    return lName;
   }
 }
