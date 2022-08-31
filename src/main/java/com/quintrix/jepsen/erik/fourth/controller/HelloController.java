12da01de8587ad12a7b8a9a7870194a2aa8a6627
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-08-31 15:01:07 -0700

    Added a requestBody parameter-using method

--- src/main/java/com/quintrix/jepsen/erik/fourth/controller/HelloController.java
+++ src/main/java/com/quintrix/jepsen/erik/fourth/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.quintrix.jepsen.erik.fourth.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +30,51 @@ public class HelloController {
   public Person postHello(@RequestParam String fName, @RequestParam Person lName) {
     lName.setFirstName(fName);
     return lName;
+  }
+
+  @PostMapping("/post-request-body")
+  @ResponseBody
+  public String postRequestBody(@RequestBody byte[] data) {
+    InputStream is = new ByteArrayInputStream(data);
+    String output = "";
+    String nextString;
+    char nextChar;
+    try {
+      int forwardTo = is.available() - 5;
+      while (is.available() > forwardTo)
+        is.read();
+      while (is.available() != 0) {
+        nextChar = (char) is.read();
+        switch (nextChar) {
+          case '+':
+            output += ' ';
+            break;
+          case '%':
+            nextString = "%" + (char) is.read();
+            nextString += (char) is.read();
+            if (nextString.equals("%0A"))
+              output += System.lineSeparator();
+            else if (nextString.equals("%2C"))
+              output += ',';
+            else if (nextString.equals("%3F"))
+              output += '?';
+            else if (nextString.equals("%27"))
+              output += '\'';
+            else if (nextString.equals("%5B"))
+              output += '[';
+            else if (nextString.equals("%5D"))
+              output += ']';
+            else
+              output += String.format("((I don't know what %s is!", nextString);
+            break;
+          default:
+            output += nextChar;
+            break;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return output;
   }
 }
